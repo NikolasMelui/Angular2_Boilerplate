63bbcdcca976fc99706d1919b8b8186f24a716fb
NikolasMelui <slak@samaradom.ru>
2016-10-29 23:43:27 +0400

    New Angular2_Boilerplate.

---
+++ app/boot.js
@@ -0,0 +1,19 @@
+System.register(['angular2/platform/browser', "./app.component"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var browser_1, app_component_1;
+    return {
+        setters:[
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            }],
+        execute: function() {
+            browser_1.bootstrap(app_component_1.AppComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJvb3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztZQUlBLG1CQUFTLENBQUMsNEJBQVksQ0FBQyxDQUFDIiwiZmlsZSI6ImJvb3QuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLy88cmVmZXJlbmNlIHBhdGg9XCIuLi8uLi9ub2RlX21vZHVsZXMvYW5ndWxhcjIvdHlwaW5ncy9icm93c2VyLmQudHNcIi8+XHJcbmltcG9ydCB7Ym9vdHN0cmFwfSBmcm9tICdhbmd1bGFyMi9wbGF0Zm9ybS9icm93c2VyJztcclxuaW1wb3J0IHtBcHBDb21wb25lbnR9IGZyb20gXCIuL2FwcC5jb21wb25lbnRcIjtcclxuXHJcbmJvb3RzdHJhcChBcHBDb21wb25lbnQpOyJdfQ==
---
+++ frontend/app_source/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>MyApp</h1>
+        <p>Description...</p>
+    `,
+})
+export class AppComponent {
+
+}
